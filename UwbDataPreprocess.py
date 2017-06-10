485e19ffb10b20ae90fa075de00898621717202a
steve <551619855@qq.com>
2017-06-10 20:02:04 +0800

    a simple filter is added.

--- UwbDataPreprocess.py
+++ UwbDataPreprocess.py
@@ -14,6 +14,7 @@ import array
 
 class UwbDataPre:
     def __init__(self,dir_name):
+        self.dir_name = dir_name
         self.file_name = dir_name
 
         self.start_time = 0.0
@@ -55,9 +56,12 @@ class UwbDataPre:
         is_first_line = True
 
         for line in this_file.readlines():
+            print("time:",line.split('[')[1].split(']')[0])
+            time_in_the_line = float(line.split('[')[1].split(']')[0])
+
             if is_first_line:
-                self.start_time -= float(line[1:10])
-                print("start time :",float(line[1:10]))
+                self.start_time -= time_in_the_line
+                print("start time :",time_in_the_line)
                 is_first_line=False
 
             if line.split(' ')[2] == '@R':
@@ -65,8 +69,8 @@ class UwbDataPre:
                 if line.split(' ')[3] != 'F1':
                     print(line)
                     mac_name = line.split(' ')[4]
-                    self.tmp_array.append(float(self.start_time+float(line[1:10])))
-                    print("time : ", float(self.start_time + float(line[1:10])))
+                    self.tmp_array.append(float(self.start_time+time_in_the_line))
+                    print("time : ", float(self.start_time + time_in_the_line))
                     for i in range(len(mac_list)):
 
                         if mac_name != mac_list[i]:
@@ -85,9 +89,17 @@ class UwbDataPre:
         # self.result_uwb[:,2] -=100.0
         # self.result_uwb[:,4]  -= 100.0
 
-        np.savetxt(dir_name+"uwb_result.csv",self.result_uwb,'%.4f',delimiter=',')
+    def save(self):
+        np.savetxt(self.dir_name+"uwb_result.csv",self.result_uwb,'%.4f',delimiter=',')
+    '''
+    
+    Show result
+    '''
+    def show(self):
+        '''
 
-
+        :return:
+        '''
         plt.figure()
         plt.plot(self.result_uwb[:,0],'r')
 
@@ -95,10 +107,48 @@ class UwbDataPre:
         plt.figure()
         plt.title('uwb ')
         for i in range(1,self.result_uwb.shape[1]):
+            # if i != 4 :
+            #     continue
             plt.plot(self.result_uwb[:,i],'*')
         plt.show()
 
         # self.start_time
+
+    def filter(self):
+        print("try to filter")
+
+        # filter for each Beacon
+        for i in range(self.result_uwb.shape[1]):
+            index_list = list()
+            # find all valid data
+            for j in range(self.result_uwb.shape[0]):
+                if self.result_uwb[j,i] > -0.1:
+                    index_list .append(j)
+
+            # Main filter process
+            step_len = 2
+            tmp_value_list = list()
+            for index in range(step_len,len(index_list)-step_len):
+                if abs((self.result_uwb[index_list[index-step_len],i]+
+                            self.result_uwb[index_list[index+step_len],i])
+                       -2.0* self.result_uwb[index_list[index],i]) > 0.12:
+                    print(abs((self.result_uwb[index_list[index-step_len],i]+
+                            self.result_uwb[index_list[index+step_len],i])
+                       -2.0* self.result_uwb[index_list[index],i]) )
+                    tmp_value_list.append(-10.0)
+                else:
+                    tmp_value_list.append(self.result_uwb[index_list[index],i])
+
+
+
+            for index in range(step_len,len(index_list)-step_len):
+                print(index-step_len,'of',len(tmp_value_list))
+                self.result_uwb[index_list[index],i] = \
+                    tmp_value_list[index-step_len]
+
+
+
+
 
 
 
@@ -108,6 +158,12 @@ class UwbDataPre:
 if __name__ == '__main__':
     # udp = UwbDataPre("/home/steve/Data/NewRecord/Record2/")
     # udp = UwbDataPre("/home/steve/tmp/test/44/")
-    udp = UwbDataPre("/home/steve/Code/Mini-IMU/Scripts/IMUWB/46/")
+    udp = UwbDataPre("/home/steve/Code/Mini_IMU/Scripts/IMUWB/46/")
+    udp.filter()
+
+    udp.save()
+    udp.show()
+
+
 
 
